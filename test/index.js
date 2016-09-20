3566613db697b756fe1664462733354553899ca1
Ryan Kois <ryan.kois@gmail.com>
2016-09-20 00:45:11 -0700

    Add test to handle invalid json stored somehow.

--- test/index.js
+++ test/index.js
@@ -4,18 +4,24 @@ var expect = require('chai').expect
 
 describe('Storage', function () {
   var fileName = './foo/bar/index.json'
-  var testStorage
+  var invalidJson = './foo/bar/invalid.json'
+  var testStorage = storage('foo', 'bar')
+
 
   /**
    * Initialize storage and save a file.
    */
-  before(function (done) {
-    testStorage = storage('foo', 'bar')
-    testStorage.save('index', {
-      foo: 'bar'
-    }, function (err) {
+  beforeEach(function (done) {
+    function writeBadJson (err) {
       if (err) return done(err)
-      done()
+      fs.writeFile(invalidJson, '{', done)
+    }
+
+    testStorage.reset(function (err) {
+      if (err) return done(err)
+      testStorage.save('index', {
+        foo: 'bar'
+      }, writeBadJson)
     })
   })
 
@@ -24,33 +30,51 @@ describe('Storage', function () {
     expect(testStorage.dir).to.equal('foo/bar')
   })
 
-  it('should save serialized storage to the data directory', function (done) {
-    fs.readFile(fileName, function (err, data) {
-      if (err) return done(err)
-      var fileData
-
-      try {
-        fileData = JSON.parse(data)
-      } catch (e) {
-        done(e)
-      }
-      done(null, fileData)
+  describe('#save', function() {
+    it('should save serialized storage to the data directory', function (done) {
+      fs.readFile(fileName, function (err, data) {
+        if (err) return done(err)
+        var fileData
+        try {
+          fileData = JSON.parse(data)
+        } catch (e) {
+          done(e)
+        }
+        done()
+      })
     })
   })
 
-  it('should retrive a stored file', function (done) {
-    testStorage.get('index', function (err, file) {
-      if (err) return done(err)
-      expect(file.foo).to.equal('bar')
-      done()
+  describe('#get', function () {
+    it('should retrive a stored file', function (done) {
+      testStorage.get('index', function (err, file) {
+        if (err) return done(err)
+        expect(file.foo).to.equal('bar')
+        done()
+      })
+    })
+
+    it('handle invalid JSON', function (done) {
+      fs.readFile(invalidJson, function (err, data) {
+        if (err) return done(err)
+        var fileData
+        try {
+          fileData = JSON.parse(data)
+        } catch (e) {
+          return done()
+        }
+        done('read the file!')
+      })
     })
   })
 
-  it('should clean the storage directory', function (done) {
-    testStorage.reset(function () {
-      fs.stat('./foo/bar', function (err, stats) {
-        if (err && err.code === 'ENOENT') return done()
-        done('file still exists!')
+  describe('#reset', function () {
+    it('should clean the storage directory', function (done) {
+      testStorage.reset(function () {
+        fs.stat('./foo/bar', function (err, stats) {
+          if (err && err.code === 'ENOENT') return done()
+          done('file still exists!')
+        })
       })
     })
   })
